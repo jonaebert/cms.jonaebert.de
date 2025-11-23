cced1c891a09c3d794a0b90f5f811fb0d53bf819
Jona Ebert <kontakt@jonaebert.de>
2025-11-23 22:52:31 +0100

    Added new collection type "event"

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -568,20 +568,21 @@ export interface ApiEventEvent extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     datetype: Schema.Attribute.Enumeration<['date-time']> &
-      Schema.Attribute.Required &
       Schema.Attribute.DefaultTo<'date-time'>;
     description: Schema.Attribute.Text;
-    end: Schema.Attribute.DateTime & Schema.Attribute.Required;
+    end: Schema.Attribute.DateTime;
+    externalEventURL: Schema.Attribute.String;
+    image: Schema.Attribute.Component<'shared.media', false>;
+    lastModifiedICS: Schema.Attribute.DateTime;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::event.event'> &
       Schema.Attribute.Private;
     location: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
-    start: Schema.Attribute.DateTime & Schema.Attribute.Required;
+    start: Schema.Attribute.DateTime;
     state: Schema.Attribute.Enumeration<
-      ['confirmed', 'tentative', 'canceled']
+      ['confirmed', 'tentative', 'cancelled']
     > &
-      Schema.Attribute.Required &
       Schema.Attribute.DefaultTo<'confirmed'>;
     subject: Schema.Attribute.String & Schema.Attribute.Required;
     uid: Schema.Attribute.UID & Schema.Attribute.Required;
